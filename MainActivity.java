af60cc1f5dbf67e46a8af078bbe6a2a7fc80bdec
Lin htet Kyaw <118300163+linhtetkyaw079@users.noreply.github.com>
2024-02-18 22:08:11 +0630

    View pager
    Uploaded 18.2.2024

---
+++ MainActivity.java
@@ -0,0 +1,174 @@
+package com.view.pager;
+
+import android.animation.*;
+import android.app.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.LinearLayout;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager.widget.ViewPager.OnAdapterChangeListener;
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+import org.json.*;
+
+public class MainActivity extends AppCompatActivity {
+	
+	private TabLayout TabLayout;
+	private LinearLayout linear1;
+	private ViewPager ViewPager;
+	
+	private FaFragmentAdapter fa;
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.main);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		TabLayout = findViewById(R.id.TabLayout);
+		linear1 = findViewById(R.id.linear1);
+		ViewPager = findViewById(R.id.ViewPager);
+		fa = new FaFragmentAdapter(getApplicationContext(), getSupportFragmentManager());
+	}
+	
+	private void initializeLogic() {
+		TabLayout.setupWithViewPager(ViewPager);
+		TabLayout.addTab(TabLayout.newTab().setText("Tab 1"));
+		TabLayout.addTab(TabLayout.newTab().setText("Tab 2"));
+		TabLayout.addTab(TabLayout.newTab().setText("Tab 3"));
+		FaFragmentAdapter fa = new FaFragmentAdapter(getApplicationContext(), getSupportFragmentManager());
+		fa.setTabCount(3);
+		ViewPager.setAdapter(fa);
+		
+		getWindow().setStatusBarColor(Color.parseColor("#000000"));
+	}
+	
+	public class FaFragmentAdapter extends FragmentStatePagerAdapter {
+		// This class is deprecated, you should migrate to ViewPager2:
+		// https://developer.android.com/reference/androidx/viewpager2/widget/ViewPager2
+		Context context;
+		int tabCount;
+		
+		public FaFragmentAdapter(Context context, FragmentManager manager) {
+			super(manager);
+			this.context = context;
+		}
+		
+		public void setTabCount(int tabCount) {
+			this.tabCount = tabCount;
+		}
+		
+		@Override
+		public int getCount() {
+			return tabCount;
+		}
+		
+		@Override
+		public CharSequence getPageTitle(int _position) {
+			if (_position == 0) {
+				return "Tab 1";
+			}
+			if (_position == 1) {
+				return "Tab 2";
+			}
+			if (_position == 2) {
+				return "Tab 3";
+			}
+			return null;
+		}
+		
+		@Override
+		public Fragment getItem(int _position) {
+			if (_position == 0) {
+				return new View1FragmentActivity();
+			}
+			if (_position == 1) {
+				return new View2FragmentActivity();
+			}
+			if (_position == 2) {
+				return new View3FragmentActivity();
+			}
+			return null;
+		}
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
---
+++ View1FragmentActivity.java
@@ -0,0 +1,55 @@
+package com.view.pager;
+
+import android.animation.*;
+import android.app.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+import org.json.*;
+
+public class View1FragmentActivity extends Fragment {
+	
+	private LinearLayout linear1;
+	private TextView textview1;
+	
+	@NonNull
+	@Override
+	public View onCreateView(@NonNull LayoutInflater _inflater, @Nullable ViewGroup _container, @Nullable Bundle _savedInstanceState) {
+		View _view = _inflater.inflate(R.layout.view1_fragment, _container, false);
+		initialize(_savedInstanceState, _view);
+		initializeLogic();
+		return _view;
+	}
+	
+	private void initialize(Bundle _savedInstanceState, View _view) {
+		linear1 = _view.findViewById(R.id.linear1);
+		textview1 = _view.findViewById(R.id.textview1);
+	}
+	
+	private void initializeLogic() {
+	}
+	
+}
